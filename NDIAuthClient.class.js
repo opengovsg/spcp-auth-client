6ca2590d1c2d17987f3618078ee61b703343d878
LoneRifle <LoneRifle@users.noreply.github.com>
2018-08-02 20:56:19 +0800

    Add eslint and de-lint accordingly

--- NDIAuthClient.class.js
+++ NDIAuthClient.class.js
@@ -1,8 +1,8 @@
-const xmlCrypto = require("xml-crypto")
-const xpath = require("xpath")
-const DOMParser = require("xmldom").DOMParser
-const xmlEnc = require("xml-encryption")
-const request = require("request")
+const xmlCrypto = require('xml-crypto')
+const xpath = require('xpath')
+const DOMParser = require('xmldom').DOMParser
+const xmlEnc = require('xml-encryption')
+const request = require('request')
 const _ = require('lodash')
 
 /**
@@ -13,22 +13,22 @@ class NDIAuthClient {
    * Creates an instance of the class
    * @param  {Object} config - Configuration parameters for instance
    */
-  constructor(config) {
+  constructor (config) {
     const PARAMS = [
-      "partnerEntityId",
-      "idpEndpoint",
-      "idpLoginURL",
-      "appKey",
-      "appCert",
-      "spcpCert",
-      "esrvcID",
+      'partnerEntityId',
+      'idpEndpoint',
+      'idpLoginURL',
+      'appKey',
+      'appCert',
+      'spcpCert',
+      'esrvcID',
     ]
 
     for (let param of PARAMS) {
       if (config[param]) {
         this[param] = config[param]
       } else {
-        throw new Error(param + " undefined")
+        throw new Error(param + ' undefined')
       }
     }
   }
@@ -38,20 +38,20 @@ class NDIAuthClient {
    * @param  {String} target - State to pass SPCP
    * @return {String} redirectURL - SPCP page to redirect to
    */
-  createRedirectURL(target) {
+  createRedirectURL (target) {
     if (!target) {
-      return new Error("Target undefined")
+      return new Error('Target undefined')
     }
     return (
       this.idpLoginURL +
-      "?RequestBinding=HTTPArtifact" +
-      "&ResponseBinding=HTTPArtifact" +
-      "&PartnerId=" +
+      '?RequestBinding=HTTPArtifact' +
+      '&ResponseBinding=HTTPArtifact' +
+      '&PartnerId=' +
       encodeURI(this.partnerEntityId) +
-      "&Target=" +
+      '&Target=' +
       encodeURI(target) +
-      "&NameIdFormat=Email" +
-      "&esrvcID=" +
+      '&NameIdFormat=Email' +
+      '&esrvcID=' +
       this.esrvcID
     )
   }
@@ -61,33 +61,33 @@ class NDIAuthClient {
    * @param  {String} xml - Xml containing artifact to be signed
    * @return {String} artifactResolve - Artifact resolve to send to SPCP
    */
-  signXML(xml) {
+  signXML (xml) {
     let sig = new xmlCrypto.SignedXml()
 
     let transforms = [
-      "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
-      "http://www.w3.org/2001/10/xml-exc-c14n#",
+      'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
+      'http://www.w3.org/2001/10/xml-exc-c14n#',
     ]
-    let digestAlgorithm = "http://www.w3.org/2001/04/xmlenc#sha256"
+    let digestAlgorithm = 'http://www.w3.org/2001/04/xmlenc#sha256'
     let xpath = "//*[local-name(.)='ArtifactResolve']"
     sig.addReference(xpath, transforms, digestAlgorithm)
 
     sig.signingKey = this.appKey
-    sig.signatureAlgorithm = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"
+    sig.signatureAlgorithm = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'
 
     let artifactResolve = null
     let signingError = null
 
     try {
-      sig.computeSignature(xml, { prefix: "ds" })
+      sig.computeSignature(xml, { prefix: 'ds' })
       artifactResolve =
         '<?xml version="1.0" encoding="UTF-8"?>' +
         '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
-        "<soapenv:Header />" +
-        "<soapenv:Body>" +
+        '<soapenv:Header />' +
+        '<soapenv:Body>' +
         sig.getSignedXml() +
-        "</soapenv:Body>" +
-        "</soapenv:Envelope>"
+        '</soapenv:Body>' +
+        '</soapenv:Envelope>'
     } catch (err) {
       signingError = err
     }
@@ -101,13 +101,13 @@ class NDIAuthClient {
    * @param  {Array} signatures - Array of signatures extracted from artifact response
    * @return {Boolean} sig0 - Boolean value of whether signatures in artifact response are verified
    */
-  verifyXML(xml, signatures) {
+  verifyXML (xml, signatures) {
     /**
      * Creates KeyInfo function
      * @param  {String} key - Public key of SPCP
      */
-    function KeyInfo(key) {
-      this.getKey = function() {
+    function KeyInfo (key) {
+      this.getKey = function () {
         return key
       }
     }
@@ -122,8 +122,8 @@ class NDIAuthClient {
     let verificationError = null
 
     // Artifact Response should contain 2 signatures
-    if (!signatures || signatures.length != 2) {
-      verificationError = "Artifact Response must contain 2 signatures"
+    if (!signatures || signatures.length !== 2) {
+      verificationError = 'Artifact Response must contain 2 signatures'
       return { isVerified, verificationError }
     }
 
@@ -152,7 +152,7 @@ class NDIAuthClient {
    * @param  {String} encryptedData - Encrypted data in artifact response from SPCP
    * @return {String} username - Decrypted username from encrypted data
    */
-  decryptXML(encryptedData) {
+  decryptXML (encryptedData) {
     let decryptedData
     let userName = null
     let decryptionError = null
@@ -161,7 +161,7 @@ class NDIAuthClient {
     }
 
     // TODO: do not mutate input variables; have separate variables for each decrypted thing
-    xmlEnc.decrypt(encryptedData.toString(), options, function(err, result) {
+    xmlEnc.decrypt(encryptedData.toString(), options, function (err, result) {
       if (err) {
         decryptionError = err
         return { userName, decryptionError }
@@ -172,7 +172,7 @@ class NDIAuthClient {
           decryptedData
         )
       }
-      xmlEnc.decrypt(encryptedData.toString(), options, function(err, result) {
+      xmlEnc.decrypt(encryptedData.toString(), options, function (err, result) {
         if (err) {
           decryptionError = err
           return { userName, decryptionError }
@@ -191,7 +191,7 @@ class NDIAuthClient {
    * @param  {String} cause - The error stack
    * @return {String} nestedError - Nested error object
    */
-  makeNestedError(errMsg, cause) {
+  makeNestedError (errMsg, cause) {
     const nestedError = new Error(errMsg)
     nestedError.cause = cause
     return nestedError
@@ -203,11 +203,11 @@ class NDIAuthClient {
    * @param  {String} relayState - State passed in on intial spcp redirect
    * @param {Function} callback - Callback function with inputs error and UserName
    */
-  getUserName(samlArt, relayState, callback) {
+  getUserName (samlArt, relayState, callback) {
     // Step 1: Check if relay state present
     if (!samlArt || !relayState) {
       callback(
-        new Error("Error in Step 1: Callback or saml artifact not present"),
+        new Error('Error in Step 1: Callback or saml artifact not present'),
         { relayState }
       )
     } else {
@@ -220,16 +220,16 @@ class NDIAuthClient {
         '" ID="_0" Version="2.0">' +
         '<saml:Issuer Format="urn:oasis:names:tc:SAML:2.0:nameid-format:entity">' +
         this.partnerEntityId +
-        "</saml:Issuer>" +
-        "<samlp:Artifact>" +
+        '</saml:Issuer>' +
+        '<samlp:Artifact>' +
         samlArt +
-        "</samlp:Artifact>" +
-        "</samlp:ArtifactResolve>"
+        '</samlp:Artifact>' +
+        '</samlp:ArtifactResolve>'
       const { artifactResolve, signingError } = this.signXML(xml)
 
       if (!artifactResolve) {
         const nestedError = this.makeNestedError(
-          "Error in Step 2: Form Artifact Resolve with Artifact and Sign",
+          'Error in Step 2: Form Artifact Resolve with Artifact and Sign',
           signingError
         )
         callback(nestedError, { relayState })
@@ -237,8 +237,8 @@ class NDIAuthClient {
         // Step 3: Send Artifact Resolve over OOB
         const requestOptions = {
           headers: {
-            "content-type": "text/xml; charset=utf-8",
-            SOAPAction: "http://www.oasis-open.org/committees/security",
+            'content-type': 'text/xml; charset=utf-8',
+            SOAPAction: 'http://www.oasis-open.org/committees/security',
           },
           url: this.idpEndpoint,
           body: artifactResolve,
@@ -246,7 +246,7 @@ class NDIAuthClient {
         request.post(requestOptions, (resolveError, response, body) => {
           if (resolveError) {
             const nestedError = this.makeNestedError(
-              "Error in Step 3: Send Artifact Resolve over OOB",
+              'Error in Step 3: Send Artifact Resolve over OOB',
               resolveError
             )
             callback(nestedError, { relayState })
@@ -264,7 +264,7 @@ class NDIAuthClient {
             )
             if (!isVerified) {
               const nestedError = this.makeNestedError(
-                "Error in Step 4: Verify Artifact Response",
+                'Error in Step 4: Verify Artifact Response',
                 verificationError
               )
               callback(nestedError, { relayState })
@@ -279,7 +279,7 @@ class NDIAuthClient {
                 callback(null, { userName, relayState })
               } else {
                 const nestedError = this.makeNestedError(
-                  "Error in Step 5: Decrypt Artifact Response",
+                  'Error in Step 5: Decrypt Artifact Response',
                   decryptionError
                 )
                 callback(nestedError, { relayState })
