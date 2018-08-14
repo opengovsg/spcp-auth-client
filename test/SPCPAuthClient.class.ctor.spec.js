8fabf5d9127e7a99115f8810be8c50a76c16c00c
LoneRifle <LoneRifle@users.noreply.github.com>
2018-08-14 23:03:33 +0800

    Add tests for ctor, jwt methods
    * Add self-signed private/public certs as test fixtures
    * Add tests for ctor and jwt methods, change `npm test` to run mocha though nyc

---
+++ test/SPCPAuthClient.class.ctor.spec.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai')
+const _ = require('lodash')
+
+const SPCPAuthClient = require('../SPCPAuthClient.class')
+
+describe('SPCPAuthClient - Constructor Tests', () => {
+  it('should reject construction if parameters missing', () => {
+    const config = {
+      partnerEntityId: 'partnerEntityId',
+      idpEndpoint: 'idpEndpoint',
+      idpLoginURL: 'idpLoginURL',
+      appKey: 'appKey',
+      appCert: 'appCert',
+      spcpCert: 'spcpCert',
+      esrvcID: 'esrvcID',
+    }
+    // create a macro function that returns a
+    // function that expect() can invoke
+    const makeClientWithout = param =>
+      () => new SPCPAuthClient(_.omit(config, param))
+
+    expect(makeClientWithout('partnerEntityId')).to.throw(Error)
+    expect(makeClientWithout('idpEndpoint')).to.throw(Error)
+    expect(makeClientWithout('idpLoginURL')).to.throw(Error)
+    expect(makeClientWithout('appKey')).to.throw(Error)
+    expect(makeClientWithout('appCert')).to.throw(Error)
+    expect(makeClientWithout('spcpCert')).to.throw(Error)
+    expect(makeClientWithout('esrvcID')).to.throw(Error)
+  })
+
+  it('should correctly construct a client', () => {
+    const config = {
+      partnerEntityId: 'partnerEntityId',
+      idpEndpoint: 'idpEndpoint',
+      idpLoginURL: 'idpLoginURL',
+      appKey: 'appKey',
+      appCert: 'appCert',
+      spcpCert: 'spcpCert',
+      esrvcID: 'esrvcID',
+    }
+    const authClient = new SPCPAuthClient(config)
+    expect(authClient.partnerEntityId).to.equal(config.partnerEntityId)
+    expect(authClient.idpEndpoint).to.equal(config.idpEndpoint)
+    expect(authClient.idpLoginURL).to.equal(config.idpLoginURL)
+    expect(authClient.appKey).to.equal(config.appKey)
+    expect(authClient.appCert).to.equal(config.appCert)
+    expect(authClient.spcpCert).to.equal(config.spcpCert)
+    expect(authClient.esrvcID).to.equal(config.esrvcID)
+    expect(authClient.jwtAlgorithm).to.equal('RS256')
+  })
+})
---
+++ test/SPCPAuthClient.class.jwt.spec.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai')
+const _ = require('lodash')
+const fs = require('fs')
+const jwt = require('jsonwebtoken')
+
+const SPCPAuthClient = require('../SPCPAuthClient.class')
+
+describe('SPCPAuthClient - JWT Tests', () => {
+  const authClient = new SPCPAuthClient({
+    partnerEntityId: 'partnerEntityId',
+    idpEndpoint: 'idpEndpoint',
+    idpLoginURL: 'idpLoginURL',
+    appKey: fs.readFileSync('./test/fixtures/certs/key.pem'),
+    appCert: fs.readFileSync('./test/fixtures/certs/server.crt'),
+    spcpCert: 'spcpCert',
+    esrvcID: 'esrvcID',
+  })
+
+  it('should create a verifiable token using jwt', () => {
+    const payload = { key: 'value' }
+    const token = authClient.createJWT(payload, '1 day')
+    const result = jwt.verify(
+      token, authClient.appCert, { algorithms: [ authClient.jwtAlgorithm ] }
+    )
+    expect(_.pick(result, 'key')).to.eql(payload)
+  })
+
+  it('should create a token it can verify', () => {
+    const payload = { key: 'value' }
+    const token = authClient.createJWT(payload, '1 day')
+    const result = authClient.verifyJWT(token)
+    expect(_.pick(result, 'key')).to.eql(payload)
+  })
+
+  it('should create a token it can verify and pass to callback', () => {
+    const payload = { key: 'value' }
+    const token = authClient.createJWT(payload, '1 day')
+    const result = authClient.verifyJWT(token, (err, data) => {
+      if (err) {
+        throw err
+      } else {
+        expect(_.pick(data, 'key')).to.eql(payload)
+      }
+    })
+    expect(result).to.equal(undefined)
+  })
+})
