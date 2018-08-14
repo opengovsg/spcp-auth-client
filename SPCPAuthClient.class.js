ac86b314381b95b0df3133d50ff780cef854aa4c
LoneRifle <LoneRifle@users.noreply.github.com>
2018-08-14 22:58:59 +0800

    Prepare codebase for testing
    * Bring in mocha, chai, nyc and supporting cast for tests
    * Adjust eslint to recognise mocha test specs
    * SPCPAuthClient - reference DOMParser through xmldom to ease testing
    * SPCPAuthClient - alter ctor jsdoc to better reflect param types for appCert/appKey
    * SPCPAuthClient.verifyJWT - return result from jwt.verify, adjust jsdoc to reflect this

--- SPCPAuthClient.class.js
+++ SPCPAuthClient.class.js
@@ -1,6 +1,6 @@
 const xmlCrypto = require('xml-crypto')
 const xpath = require('xpath')
-const DOMParser = require('xmldom').DOMParser
+const xmldom = require('xmldom')
 const xmlEnc = require('xml-encryption')
 const request = require('request')
 const _ = require('lodash')
@@ -18,8 +18,8 @@ class SPCPAuthClient {
    * @param  {String} config.idpLoginURL - the fully-qualified SingPass/CorpPass IDP url to redirect login attempts to
    * @param  {String} config.idpEndpoint - the fully-qualified SingPass/CorpPass IDP url for out-of-band (OOB) authentication
    * @param  {String} config.esrvcID - the e-service identifier registered with SingPass/CorpPass
-   * @param  {String} config.appCert - the e-service public certificate issued to SingPass/CorpPass
-   * @param  {String} config.appKey - the e-service certificate private key
+   * @param  {(String|Buffer)} config.appCert - the e-service public certificate issued to SingPass/CorpPass
+   * @param  {(String|Buffer)} config.appKey - the e-service certificate private key
    * @param  {String} config.spcpCert - the public certificate of SingPass/CorpPass, for OOB authentication
    */
   constructor (config) {
@@ -84,10 +84,16 @@ class SPCPAuthClient {
   /**
    * Verifies a JWT for SingPass/CorpPass-authenticated session
    * @param  {String} jwtToken - The JWT to verify
-   * @param  {Function} callback - Callback called with decoded payload
+   * @param  {Function} callback - Optional - Callback called with decoded payload
+   * @return {Object} the decoded payload if no callback supplied, or nothing otherwise
    */
   verifyJWT (jwtToken, callback) {
-    jwt.verify(jwtToken, this.appCert, { algorithms: [this.jwtAlgorithm] }, callback)
+    return jwt.verify(
+      jwtToken,
+      this.appCert,
+      { algorithms: [this.jwtAlgorithm] },
+      callback
+    )
   }
 
   /**
@@ -200,7 +206,7 @@ class SPCPAuthClient {
         decryptionError = err
         return { userName, decryptionError }
       } else {
-        decryptedData = new DOMParser().parseFromString(result)
+        decryptedData = new xmldom.DOMParser().parseFromString(result)
         encryptedData = xpath.select(
           "//*[local-name(.)='EncryptedData']",
           decryptedData
@@ -211,7 +217,7 @@ class SPCPAuthClient {
           decryptionError = err
           return { userName, decryptionError }
         } else {
-          decryptedData = new DOMParser().parseFromString(result)
+          decryptedData = new xmldom.DOMParser().parseFromString(result)
           userName = _.get(decryptedData, ['documentElement', 'childNodes', '0', 'data'], null)
         }
       })
@@ -287,7 +293,7 @@ class SPCPAuthClient {
           } else {
             // Step 4: Verify Artifact Response
             let responseXML = body
-            let responseDOM = new DOMParser().parseFromString(responseXML)
+            let responseDOM = new xmldom.DOMParser().parseFromString(responseXML)
             let signatures = xpath.select(
               "//*[local-name(.)='Signature']",
               responseDOM
