70e9b413a4db3c9b942eb2a841dfd665541ed807
LoneRifle <LoneRifle@users.noreply.github.com>
2018-09-13 09:12:14 +0800

    Add tests for redirection

---
+++ test/SPCPAuthClient.class.redirect.spec.js
@@ -0,0 +1,48 @@
+const { expect } = require('chai')
+const SPCPAuthClient = require('../SPCPAuthClient.class')
+
+describe('SPCPAuthClient - createRedirectURL', () => {
+  const authClient = new SPCPAuthClient({
+    partnerEntityId: 'partnerEntityId',
+    idpEndpoint: 'idpEndpoint',
+    idpLoginURL: 'idpLoginURL',
+    appKey: 'appKey',
+    appCert: 'appCert',
+    spcpCert: 'spcpCert',
+    esrvcID: 'esrvcID',
+  })
+  it('should return Error on no target', () => {
+    expect(authClient.createRedirectURL()).to.be.instanceOf(Error)
+  })
+  it('should return redirect URL with default e-service id', () => {
+    const target = 'target'
+    expect(authClient.createRedirectURL(target)).to.equal(
+      authClient.idpLoginURL +
+      '?RequestBinding=HTTPArtifact' +
+      '&ResponseBinding=HTTPArtifact' +
+      '&PartnerId=' +
+      encodeURI(authClient.partnerEntityId) +
+      '&Target=' +
+      encodeURI(target) +
+      '&NameIdFormat=Email' +
+      '&esrvcID=' +
+      authClient.esrvcID
+    )
+  })
+  it('should return redirect URL with specified e-service id', () => {
+    const target = 'target'
+    const esrvcID = 'otherId'
+    expect(authClient.createRedirectURL(target, esrvcID)).to.equal(
+      authClient.idpLoginURL +
+      '?RequestBinding=HTTPArtifact' +
+      '&ResponseBinding=HTTPArtifact' +
+      '&PartnerId=' +
+      encodeURI(authClient.partnerEntityId) +
+      '&Target=' +
+      encodeURI(target) +
+      '&NameIdFormat=Email' +
+      '&esrvcID=' +
+      esrvcID
+    )
+  })
+})
